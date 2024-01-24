05b86b74a1bf65de61588fcbfbc6ae466a86c510
Melik Şah ATALAY <meliksah.atalay@gmail.com>
2024-01-24 11:19:45 +0300

    Proje dosyası ekle.

---
+++ Consollaplication/CustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consollaplication
+{
+    internal class CustomerManager
+    {
+      public void AddCustomer(Class1 customer)
+        {
+            Console.WriteLine("Eklenen Müşteri: " + customer.Name + "" + customer.Surname);
+        }
+        public void RemoveCustomer(Class1 customer2)
+        {
+            Console.WriteLine("Silinen Müşteri: " + customer2.Name + "" + customer2.Surname);
+        }
+        public void CustomerList(Class1[] customers)
+        {
+           foreach(var item in customers)
+            {
+                Console.WriteLine("Müşteri: " + item.Name + " " + item.Surname);
+            }
+        }
+    }
+
+}
---
+++ Consollaplication/Program.cs
@@ -0,0 +1,33 @@
+﻿using Consollaplication;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Class1 customer = new Class1();
+
+        customer.Surname = "ATALAY";
+        customer.Name = "Melik Şah";
+        customer.TcNo = "147258369";
+        customer.Id = 1;
+
+        Class1 customer2 = new Class1();
+
+        customer2.Name = "Melik Şah";
+        customer2.Surname = "Atalay";
+        customer2.Id = 3;
+        customer2.TcNo = "123456789";
+
+        //Add Customer 
+
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.AddCustomer(customer)
+;
+        //Remove Customer 
+        customerManager.RemoveCustomer(customer2);
+
+        Class1[] customerList = new Class1[] { customer, customer2 };
+
+        customerManager.CustomerList(customerList);
+    }
+}
